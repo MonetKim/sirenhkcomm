8bc6e8b7768ff9e69e7ea6ca740a33c11f0cd92f
DESKTOP-KH7FTRG\suser <neidmuya@naver.com>
2021-08-24 09:25:12 +0900

    0824 비디오추가

--- src/screens/LoginScreen/login.js
+++ src/screens/LoginScreen/login.js
@@ -28,7 +28,7 @@ const LoginScreen = () => {
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
         <Video
-        source={require("../../../assets//video/hksample.mp4")}
+        source={require("../../../assets//video/paran.mp4")}
         //source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
         rate={1.0}
         volume={1.0}
